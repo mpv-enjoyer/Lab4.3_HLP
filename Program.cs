3ac1d8257029ffadf94dc1cc18eaa8684a9fa4ae
mpv-enjoyer <Disblocker.just@gmail.com>
2023-11-11 13:36:32 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Expression: ");
+string? input = Console.ReadLine();
+CheckData output = Check(input);
+if (output.LeftUnmatched == null && output.RightUnmatched == null)
+{
+    Console.WriteLine("Match.");
+    return;
+}
+if (output.LeftUnmatched != null)
+{
+    Console.WriteLine($"Left unmatch: {output.LeftUnmatched}");
+    return;
+}
+Console.WriteLine($"Right unmatch: {output.RightUnmatched}");
+
+CheckData Check(string? input)
+{
+    CheckData output = new CheckData();
+    if (input == null) return output;
+    bool inside = false;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == '(')
+        {
+            if (inside && output.LeftUnmatched == null)
+            {
+                output.LeftUnmatched = i;
+            }
+            inside = true;
+        }
+        if (input[i] == ')')
+        {
+            if (!inside && output.RightUnmatched == null)
+            {
+                output.RightUnmatched = i;
+            }
+            inside = false;
+        }
+    }
+    return output;
+}
+struct CheckData
+{
+    public int? LeftUnmatched;
+    public int? RightUnmatched;
+    public CheckData() { LeftUnmatched = null; RightUnmatched = null; }
+};
